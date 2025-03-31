4699dfad89cb83e4d5a7426e04c0313440cc423e
KaviyarasanK1919 <kaviyarasan.koodalingam@gmail.com>
2025-03-31 18:11:19 +0530

    Update EmployeeController.java

--- task10/employee/EmployeeController.java
+++ task10/employee/EmployeeController.java
@@ -1,66 +1,14 @@
 package task10.employee;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-import Task6ClassAndObject.Employee;
-
 public class EmployeeController {
 
-	static int employeeID = 1;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		List<Employee> employeeList = new ArrayList<>();
 		
-		Scanner scan = new Scanner(System.in);
-		boolean run = true;
-		while(run)
-		{
-			System.out.println("1.Add Employee.");
-			System.out.println("2.Show All Employees.");
-			System.out.println("3.Exit");
-			
-			int choice = scan.nextInt();
-			scan.nextLine();
-			switch(choice)
-			{
-			case 1:
-				
-				System.out.println("        ------         Enter Employee Details        ------");
-				System.out.println("Enter Name : ");
-				String name = scan.nextLine();
-				System.out.println("Enter Department : ");
-				String deptName = scan.nextLine();
-				System.out.println("Enter Salary : ");
-				double salary = scan.nextDouble();
-				scan.nextLine();
-				System.out.println("Enter Email : ");
-				String email = scan.nextLine();
-				System.out.println("Enter MobileNumber : ");
-				String mobileNumber = scan.nextLine();
-				
-				employeeList.add(new Employee(employeeID++ +"", name, deptName, salary, email, mobileNumber));
-				
-				break;
-				
-			case 2:
-				for(Employee i : employeeList)
-				{
-					System.out.println(i);
-				}
-				break;
-				
-			case 3:
-				run = false;
-				break;
-				
-			default :
-				System.out.println("Please Enter valid option.");
-				break;
-			}
-		}
-		scan.close();
+		Employee salariedEmployee = new SalariedEmployee("Z-S-1900", "kavi", "Arattai", "936123****", "kavi@gmail.com", "Kavi193e!#r", 100000, "Honorable Employee");
+		
+		Employee hourlyEmployee = new HourlyEmployee("Zk-e-134", "vishwa", "SalesIQ", "987654****", "vishwa@gmail.com", "Visj123@@!", 345600, 10);
+		
+		salariedEmployee.getEmployeeDetails();
+		hourlyEmployee.getEmployeeDetails();
 	}
-
 }
